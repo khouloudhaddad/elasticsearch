ba91890ff2c4ca864ac20d4feed1ae4a2d64ef66
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-08-04 21:34:36 +0100

    Search form

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ RUN apt-get update \
     && apt-get install -y --no-install-recommends git libicu-dev libpng-dev libxml2-dev libzip-dev libonig-dev libxslt-dev zip \
     && docker-php-ext-configure intl \
     && docker-php-ext-install pdo pdo_mysql gd opcache intl zip calendar dom mbstring xsl \
-    && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+    && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+
+RUN echo 'max_execution_time = 120' >> /usr/local/etc/php/conf.d/docker-php-maxexectime.ini;
 
 WORKDIR /app
 
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Form\Type\SearchFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+
+        return $this->render('search/index.html.twig',
+        [
+            'form' => $form->createView()
         ]);
     }
 }
